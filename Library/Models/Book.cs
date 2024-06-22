f2d4eb3e58823f1b2c886da2b4bcf60dd0fe3c90
Khalid Nidal <khalidnidal541@gmail.com>
2024-06-22 19:58:06 +0300

    first

---
+++ Library/Models/Book.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+
+
+        [Required]
+        [StringLength(20, MinimumLength = 3)]
+        public string bookTitle { get; set; } = string.Empty;
+
+
+
+        [Required]
+        [StringLength(200, MinimumLength = 3)]
+        public string bookDesc { get; set; } = string.Empty;
+
+
+        [Required]
+        public DateTime bookWritingDate { get; set; }
+
+
+        [Required]
+        public int AuthorId { get; set; }
+
+        public Author Author { get; set; }
+
+
+
+    }
+}
